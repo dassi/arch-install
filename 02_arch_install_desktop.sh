331bbafd3b9f3e135e20993dd12aa60eca3d9864
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2025-01-03 10:58:42 +0100

    sshd service

--- 02_arch_install_desktop.sh
+++ 02_arch_install_desktop.sh
@@ -187,6 +187,10 @@ sudo systemctl enable --now cronie
 # Enable TRIM-ing SSD devices periodically
 sudo systemctl enable --now fstrim.timer
 
+# Start SSH daemon
+sudo systemctl enable --now sshd.service
+
+
 # This line, overwriting the `newperms` command above will allow the user to run
 # serveral important commands, `shutdown`, `reboot`, updating, etc. without a password.
 newPerms "%wheel ALL=(ALL) ALL #FROM_INSTALL_SCRIPT
